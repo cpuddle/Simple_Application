55384ecc580117b6b25f46d5e1dd719cd2fce34d
cpuddle <collin.franklin.2003@gmail.com>
2023-12-18 20:27:34 -0600

    Update

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_video.h>
+#include <SDL2/SDL_events.h>
 
-int main() {
-    printf("Hello World!\n");
-    return 0;
+int main(int argc, char ** argv) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *win = SDL_CreateWindow("title", 30, 30, 600, 500, SDL_WINDOW_SHOWN);
+    SDL_Surface * screen = SDL_GetWindowSurface(win);
+    bool run = true;
+    while(run) {
+        SDL_Event ev;
+        while(SDL_PollEvent(&ev)) {
+            switch(ev.type) {
+                case SDL_QUIT:
+                    run = false;
+                    break;
+            }
+        }
+    }
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 }
